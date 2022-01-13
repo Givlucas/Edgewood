562d4516ef9e2df71b48767bc12b18bf7119dab7
lucas <givlucas@gmail.com>
2022-01-13 14:50:14 -0500

    Building nav button

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('layout', { title: 'Main Page' });
 });
 
 router.get('/express', function(req, res, next) {
