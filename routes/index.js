e607d24cc49b71a28badf39b2a7f8488537af0aa
lucas <givlucas@gmail.com>
2022-01-17 11:36:28 -0500

    Switch routes from layout to begin setting up main home page

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('layout', { title: 'Main Page' });
+  res.render('index', { title: 'Main Page' });
 });
 
 router.get('/express', function(req, res, next) {
-  res.render('index1', { title: 'Express' });
+  res.render('', { title: 'Express' });
 });
 
 module.exports = router;
