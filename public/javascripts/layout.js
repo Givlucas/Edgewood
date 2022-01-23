46b0e8eb8ee7f68606496fa6482eea384f4cbf38
lucas <givlucas@gmail.com>
2022-01-23 17:53:09 -0500

    changed nav up a bit

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -19,17 +19,22 @@ window.onscroll = function() {stickyHeader()};
 var header = document.getElementById("head");
 var buffer = document.getElementById("buff");
 var img = document.getElementById("img");
+var title = document.getElementById("title");
 
 var sticky = header.offsetTop;
 
 function stickyHeader() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
+    header.classList.add("shrink-txt");
     buffer.classList.add("shrink");
+    title.classList.add("grow");
     img.classList.add("shrink");
   } else {
     header.classList.remove("sticky");
+    header.classList.remove("shrink-txt");
     buffer.classList.remove("shrink");
+    title.classList.remove("grow");
     img.classList.remove("shrink");
   }
 }
